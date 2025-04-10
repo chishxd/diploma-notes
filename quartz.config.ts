58d6e7c5e90988d02a27abc04026ed19a623039a
chishxd <chishxd@proton.me>
2025-04-10 20:30:18 +0530

    Quartz sync: Apr 10, 2025, 8:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Chinmay's Diplma Notes",
+    pageTitle: "Chinmay's Diploma Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
