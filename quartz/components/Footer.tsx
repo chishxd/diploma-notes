bcccf9a3db41ac9f3851428d89f0cb45fef6b218
Chinmay Shet <chishxd@tutamail.com>
2025-06-16 17:25:32 +0530

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -11,12 +11,15 @@ export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
+
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          <a href="https://github.com/chishxd">GitHub</a> <br> <a href="https://www.linkedin.com/in/chinmay-shet0">LinkedIn</a>
+          <a href="https://github.com/chishxd">GitHub</a>
+          <br />
+          <a href="https://www.linkedin.com/in/chinmay-shet0">LinkedIn</a>
         </p>
-        </footer>
+      </footer>
     )
   }
 
